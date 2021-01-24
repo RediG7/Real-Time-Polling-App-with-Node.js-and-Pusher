fccca546c17eb0d3acdb7ae7cc3be8932569f00f
RediG7 <guleksiredi7@gmail.com>
2021-01-24 01:26:46 +0100

    Initial Server Config

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();;
+
+const poll = require('./routes/poll');
+
+// Set public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Enable CORS
+app.use(cors());
+// anything requested in /poll will be reflected in poll file
+app.use('/poll', poll);
+
+const PORT = process.env.PORT || 3000;
+
+// Start server
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
