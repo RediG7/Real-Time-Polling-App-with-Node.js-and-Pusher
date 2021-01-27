1056981dd9f3b18503547e4c9358176d8cd6cd47
RediG7 <guleksiredi7@gmail.com>
2021-01-27 18:54:23 +0100

    Add functionality(count total votes for each os)

--- public/main.js
+++ public/main.js
@@ -1,69 +1,81 @@
-const form = document.getElementById('vote-form');
+const form = document.getElementById("vote-form");
 
 // Form Submit Event
-form.addEventListener('submit', (e) => {
-    const choice = document.querySelector('input[name=os]:checked').value;
-    const data = {os: choice};
+form.addEventListener("submit", (e) => {
+  const choice = document.querySelector("input[name=os]:checked").value;
+  const data = { os: choice };
 
-    fetch('http://localhost:3000/poll', {
-        method: 'post',
-        body: JSON.stringify(data),
-        headers: new Headers({
-            'Content-Type': 'application/json'
-        })
-    })
-    .then(res => res.json()) // fetch returns a promise
-    .then(data => console.log(data)) // needs to .then()
-    .catch(err => console.log(err));
+  fetch("http://localhost:3000/poll", {
+    method: "post",
+    body: JSON.stringify(data),
+    headers: new Headers({
+      "Content-Type": "application/json",
+    }),
+  })
+    .then((res) => res.json()) // fetch returns a promise
+    .then((data) => console.log(data)) // needs to .then()
+    .catch((err) => console.log(err));
 
-    e.preventDefault();
+  e.preventDefault();
 });
 
-let dataPoints = [
-    { label: 'Windows', y: 0},
-    { label: 'MacOS', y:0 },
-    { label: 'Linux', y: 0 },
-    { label: 'Other', y: 0 }
-];
+fetch("http://localhost:3000/poll")
+  .then((res) => res.json())
+  .then((data) => {
+    const votes = data.votes;
+    const totalVotes = votes.length;
+    // Count vote points (for each one) - reduce(acumulator, current value)
+    const voteCounts = votes.reduce(
+      (acc, vote) =>
+        ((acc[vote.os] = (acc[vote.os] || 0) + parseInt(vote.points)),acc),{});
 
-const chartContainer = document.querySelector('#chartContainer');
+    let dataPoints = [
+      { label: "Windows", y: voteCounts.Windows },
+      { label: "MacOS", y: voteCounts.MacOS },
+      { label: "Linux", y: voteCounts.Linux },
+      { label: "Other", y: voteCounts.Other },
+    ];
 
-if (chartContainer) {
-    const chart = new CanvasJS.Chart('chartContainer', {
+    const chartContainer = document.querySelector("#chartContainer");
+
+    if (chartContainer) {
+      const chart = new CanvasJS.Chart("chartContainer", {
         animationEnabled: true,
-        theme: 'theme1',
+        theme: "theme1",
         title: {
-            text: 'OS Results'
+          text: `Total Votes ${totalVotes}`,
         },
         data: [
-            {
-                type: 'column',
-                dataPoints: dataPoints
-            }
-        ]
-    });
-    chart.render();
+          {
+            type: "column",
+            dataPoints: dataPoints,
+          },
+        ],
+      });
+      chart.render();
 
-    // Enable pusher logging - don't include this in production
-    Pusher.logToConsole = true;
+      // Enable pusher logging - don't include this in production
+      Pusher.logToConsole = true;
 
-    var pusher = new Pusher('f333025552436769a36a', { // APP_ID can be seen on view
-      cluster: 'eu',
-      encrypted: true
-    });
- 
-    var channel = pusher.subscribe('os-poll');
-    channel.bind('os-vote', function(data) {
-        dataPoints = dataPoints.map(x => {
-            if (x.label == data.os) {
-                x.y += data.points;
-                return x;
-            } else {
-                return x;
-            }
+      var pusher = new Pusher("f333025552436769a36a", {
+        // APP_ID can be seen on view
+        cluster: "eu",
+        encrypted: true,
+      });
+
+      var channel = pusher.subscribe("os-poll");
+      channel.bind("os-vote", function (data) {
+        dataPoints = dataPoints.map((x) => {
+          if (x.label == data.os) {
+            x.y += data.points;
+            return x;
+          } else {
+            return x;
+          }
         });
         chart.render();
-    });
-}
+      });
+    }
+  });
 
 // Front end side
