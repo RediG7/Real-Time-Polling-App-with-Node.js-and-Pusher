fcd7dd435169b9ae80f059b27704e283d22dedfb
RediG7 <guleksiredi7@gmail.com>
2021-01-27 18:52:14 +0100

    Save/show data To/from db

--- routes/poll.js
+++ routes/poll.js
@@ -1,29 +1,45 @@
-require('dotenv').config()
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 
-const Pusher = require('pusher');
+const Vote = require("../models/Vote");
+
+const Pusher = require("pusher");
 
 const pusher = new Pusher({
-    appId: process.env.APP_ID,
-    key: process.env.KEY,
-    secret: process.env.SECRET,
-    cluster: process.env.CLUSTER,
-    useTLS: true
-  });
-
-// this means /poll because we set -> app.use('/poll', poll);
-router.get('/', (req, res) => {
-    res.send('POLL');
+  appId: process.env.APP_ID,
+  key: process.env.KEY,
+  secret: process.env.SECRET,
+  cluster: process.env.CLUSTER,
+  useTLS: true,
 });
 
-router.post('/', (req, res) => {
-    pusher.trigger("os-poll", "os-vote", {
-        points: 1,
-        os: req.body.os
-      });
+// this means /poll because we set -> app.use('/poll', poll);
+router.get("/", (req, res) => {
+  Vote.find().then(votes => {
+    res.json({success: true,
+    votes: votes})
+  });
+});
 
-      return res.json({success: true, message: 'Thank you for voting'});
+router.post("/", (req, res) => {
+  const newVote = {
+    os: req.body.os,
+    points: 1,
+  };
+
+  new Vote(newVote).save().then((vote) => {
+    pusher.trigger("os-poll", "os-vote", {
+      points: parseInt(vote.points),
+      os: vote.os,
+    });
+
+    return res.json({
+      success: true,
+      message: "Thank you for voting",
+    });
+  });
 });
 
 module.exports = router;
